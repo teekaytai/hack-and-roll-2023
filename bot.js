e3bd047382458163cc8575337195cb1fee70c156
teekaytai <90845551+teekaytai@users.noreply.github.com>
2023-01-14 14:39:53 +0800

    Add alert command

--- bot.js
+++ bot.js
@@ -32,12 +32,26 @@ const commands = [
                 required: false,
                 min_value: 1,
                 max_value: 5
-            },
+            }
+        ]
+    },
+    {
+        name: 'alert',
+        description: 'Sets an alert for a Carousell search',
+        options: [
             {
                 type: 3,
-                name: 'filters',
-                description: 'Filters?',
-                required: false
+                name: 'name',
+                description: 'Name of the item',
+                required: true
+            },
+            {
+                type: 4,
+                name: 'count',
+                description: 'Number of results',
+                required: false,
+                min_value: 1,
+                max_value: 5
             }
         ]
     }
@@ -77,6 +91,21 @@ client.on('interactionCreate', async interaction => {
             s += '\n' + hyperlink(results[i].title, "https://www.carousell.sg/p/" + results[i].id);
         }
         await interaction.reply(`Search: ${query}, with filters: ${filters}\nResults: ${s}`);
+    } else if (interaction.commandName === 'alert') {
+        const query = interaction.options.get('name')?.value;
+        const count = interaction.options.get('count')?.value ?? 1;
+        const filters = interaction.options.get('filters')?.value;
+        const alert = async function() {
+            console.log(`alert ${query}`);
+            const results = await search(query, count);
+            let s = "";
+            for (let i = 0; i < results.length; ++i) {
+                s += '\n' + hyperlink(results[i].title, "https://www.carousell.sg/p/" + results[i].id);
+            }
+            await interaction.channel.send(`Search: ${query}, with filters: ${filters}\nResults: ${s}`);
+        }
+        await interaction.reply(`Ok got it`);
+        setInterval(alert, 5000);
     }
 });
 
