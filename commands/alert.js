eff46dae969c3df17ce3738e2da47ef754b1a887
teekaytai <90845551+teekaytai@users.noreply.github.com>
2023-01-14 17:01:12 +0800

    Save user alerts

--- commands/alert.js
+++ commands/alert.js
@@ -2,7 +2,7 @@ import { search } from '../api.js';
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const { hyperlink } = require('discord.js');
-import { getUser, setUser } from '../db.js';
+import { getUser } from '../db.js';
 
 export const NAME = 'alert';
 export const DESCRIPTION = 'Sets an alert for a Carousell search';
@@ -16,8 +16,13 @@ export const OPTIONS = [
 ]
 
 export async function run(interaction) {
+    const user = interaction.user;
     const query = interaction.options.get('name')?.value;
     const filters = interaction.options.get('filters')?.value;
+    if (query in getUser(user)) {
+        return `You already set an alert for ${query}`;
+    }
+
     let lastResultTime = 0;
     const alert = async function () {
         console.log(`alert ${query}`);
@@ -37,11 +42,11 @@ export async function run(interaction) {
         lastResultTime = Math.max(lastResultTime, latestResultTime);
         const resultsString = newResults.map(result => hyperlink(result.title, "https://www.carousell.sg/p/" + result.id)).join('\n')
         if (newResults.length > 0) {
-            interaction.channel.send(`${interaction.user}\nSearch: ${query}, with filters: ${filters}\nResults: ${resultsString}`);
+            interaction.channel.send(`${user}\nSearch: ${query}, with filters: ${filters}\nResults: ${resultsString}`);
         } else {
             console.log(`No new results for ${query}`);
         }
     }
-    setInterval(alert, 5000);
+    getUser(user)[query] = setInterval(alert, 5000);
     return `Alert set for '${query}'`;
 }
