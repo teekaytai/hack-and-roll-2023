4dd0029f169300e25f287803485cc4cb7a735e20
tsammeow <97447335+tsammeow@users.noreply.github.com>
2023-01-14 12:37:53 +0800

    format

--- test.js
+++ test.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
 
- async function getTokens(site) {
-  let res = await axios.get(
-    site
-  );
-  const tokens = [];
-  tokens.push(res.headers['set-cookie'][0].split(";")[0]);
-  tokens.push(res.data.substring(res.data.search("csrfToken")).split(",")[0].replace(/"/g,'').split(":")[1]);
-  return tokens;
+async function getTokens(site) {
+    let res = await axios.get(
+        site
+    );
+    const tokens = [];
+    tokens.push(res.headers['set-cookie'][0].split(";")[0]);
+    tokens.push(res.data.substring(res.data.search("csrfToken")).split(",")[0].replace(/"/g, '').split(":")[1]);
+    return tokens;
 }
 
- async function getResponse(site,body,headers) {
-  let res = await axios.post(
-      site, body, { headers }
+async function getResponse(site, body, headers) {
+    let res = await axios.post(
+        site, body, { headers }
     );
     const text = res.data;
     return text;
 }
 
- function getData(query, fields){
+function getData(query, fields) {
     let l = query.data.results.length;
     let data = [];
-    for(let i = 0; i<l; i++){
-      let temp = [];
-      for(let j = 0; j<fields.length; j++){
-        temp[j] = query.data.results[i].listingCard[fields[j]];
+    for (let i = 0; i < l; i++) {
+        let temp = [];
+        for (let j = 0; j < fields.length; j++) {
+            temp[j] = query.data.results[i].listingCard[fields[j]];
         }
         data[i] = temp;
     }
@@ -35,22 +35,22 @@ import axios from 'axios';
 
 
 const body = {
-  "bestMatchEnabled": true,
-  "canChangeKeyword": true,
-  "count": 40,
-  "countryCode": "SG",
-  "countryId": "1880251",
-  "filters": [],
-  "includeEducationBanner": true,
-  "includeSuggestions": true,
-  "locale": "en",
-  "prefill": {},
-  "query": "PS5"
+    "bestMatchEnabled": true,
+    "canChangeKeyword": true,
+    "count": 40,
+    "countryCode": "SG",
+    "countryId": "1880251",
+    "filters": [],
+    "includeEducationBanner": true,
+    "includeSuggestions": true,
+    "locale": "en",
+    "prefill": {},
+    "query": "PS5"
 };
 
-const headers = { 
+const headers = {
     'cookie': "",
-  'csrf-token': ""
+    'csrf-token': ""
 };
 
 
@@ -58,10 +58,10 @@ const tokens = await getTokens("https://carousell.sg");
 headers['cookie'] = tokens[0];
 headers['csrf-token'] = tokens[1];
 body['query'] = "PS5		";
-const response = await getResponse('https://www.carousell.sg/api-service/filter/cf/4.0/search/',body,headers);
-const fields = ["title","price"]
-const data = getData(response,fields);
+const response = await getResponse('https://www.carousell.sg/api-service/filter/cf/4.0/search/', body, headers);
+const fields = ["title", "price"]
+const data = getData(response, fields);
 
-for(let i = 0; i<data.length; i++){
-  console.log(data[i]);
+for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
 }
